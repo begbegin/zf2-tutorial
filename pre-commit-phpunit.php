478a1219b9071957ef221cd5fba34484778f596a
W. Begbessou <w.begbessou@ingeniousafrica.com>
2015-03-27 11:32:23 +0000

    Test commit

--- pre-commit-phpunit.php
+++ pre-commit-phpunit.php
@@ -1,8 +1,11 @@
 <?php
 echo PHP_EOL;
-// output a little introduction
 echo '>> Unit tests ongoing...' . PHP_EOL;
+
 exec('D:\Users\w.begbessou\AppData\Roaming\Composer\vendor\bin\phpunit', $output, $returnCode); 
 echo implode(PHP_EOL, $output);
+
+echo PHP_EOL;
 echo '>> End unit tests' . PHP_EOL;
+
 die($returnCode);
